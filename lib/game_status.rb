967470cf99bca1434bb6218d2037ab2ed180519a
Morgan Matney <morgan.matney89@gmail.com>
2020-05-01 16:37:15 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -4,3 +4,40 @@ def position_taken?(board, index)
 end
 
 # Define your WIN_COMBINATIONS constant
+WIN_COMBINATIONS = [
+  [0, 1, 2], #Top row
+  [3, 4, 5], #Middle row
+  [6, 7, 8], #Bottom row
+  [0, 3, 6], #First column
+  [1, 4, 7], #Second column
+  [2, 5, 8], #Third column
+  [0, 4, 8], #Diagonal
+  [2, 4, 6], #Diagonal
+]
+
+#Winner?
+def won?(board)
+  if board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+    return false
+  else
+    return true
+  end
+end
+
+#Full Board?
+def full?(board)
+  if board.detect{|i| i = " "}
+    return false
+  else
+    return true
+  end
+end
+
+#Draw?
+def draw?(board)
+  if won?(board) == false && full?(board) == true
+    return true
+  else
+    return false
+  end
+end
